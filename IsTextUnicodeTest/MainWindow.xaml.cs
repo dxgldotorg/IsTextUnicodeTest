e978b0cd2c65d48e2583856884de176a05d62e58
dxgldotorg <william@dxgl.org>
2025-03-22 21:06:15 -0400

    Add project files.

---
+++ IsTextUnicodeTest/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace IsTextUnicodeTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int flags = ~0;
+            [DllImport("Advapi32", SetLastError = false)]
+            static extern bool IsTextUnicode(byte[] buf, int len, ref int opt);
+            byte[] ansitext = Encoding.Default.GetBytes(TextInput.Text);
+            bool isunicode = IsTextUnicode(ansitext, ansitext.Length, ref flags);
+            if (isunicode)
+            {
+                OutputLabel.Content = "Text (detected as Unicode)";
+                TextOutput.Text = System.Text.Encoding.Unicode.GetString(ansitext);
+            }
+            else
+            {
+                OutputLabel.Content = "Text (detected as ANSI)";
+                TextOutput.Text = System.Text.Encoding.Default.GetString(ansitext);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# IsTextUnicodeTest
+
+![Screenshot of IsTextUnicodeTest](https://github.com/dxgldotorg/IsTextUnicodeTest/blob/master/MainWindow.png?raw=true)
+
+This is a simple program designed to demonstrate the IsTextUnicode API and how it affected the loading of plain text files in Notepad for Windows XP and below.
+Program requires .NET 8.0 Windows Desktop Runtime but it uses the same Win32 API that Notepad used back in the day, and has the same glitches even in modern Windows versions like Windows 11.
+
+This program is public domain and intended for educational purposes.
